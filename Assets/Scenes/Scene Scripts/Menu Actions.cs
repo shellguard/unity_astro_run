fc65a1fae7b491d7bada6adb53f9196ecd8eb08e
Andrew Popovych <andrew.popovych@finning.com>
2019-01-20 19:20:00 -0800

    Added Project Files

---
+++ Assets/Scenes/Scene Scripts/Menu Actions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Forest");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
